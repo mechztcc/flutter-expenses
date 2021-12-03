d5d65332d3d4107aa38374a6b9ebb080b959dfc2
mechztcc <dojathan@gmail.com>
2021-12-03 11:10:50 -0300

    aula 94

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:financeiro/models/transaction.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -8,14 +9,21 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+
+  final _transactions = [
+    Transaction(
+        id: 't1', title: 'Novo Tênis', value: 310.76, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Conta de luz', value: 200.76, date: DateTime.now())
+  ];
 
   @override
   Widget build(BuildContext context) {
