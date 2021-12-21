1c6dd1a511622c35c414c84a400a26a5c2a74863
mechztcc <dojathan@gmail.com>
2021-12-21 11:18:15 -0300

    responsividade vertical

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -12,45 +12,43 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 350,
-        child: transactions.isEmpty
-            ? Column(children: [
-                const SizedBox(height: 50),
-                Text('Nenhuma transação cadastrada',
-                    style: Theme.of(context).textTheme.headline6),
-              ])
-            : ListView.builder(
-                itemCount: transactions.length,
-                itemBuilder: (ctx, index) {
-                  final tr = transactions[index];
-                  return Card(
-                    elevation: 5,
-                    margin: EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 20,
+    return transactions.isEmpty
+        ? Column(children: [
+            const SizedBox(height: 50),
+            Text('Nenhuma transação cadastrada',
+                style: Theme.of(context).textTheme.headline6),
+          ])
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 20,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Theme.of(context).primaryColor,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: FittedBox(child: Text('R\$${tr.value}')),
                     ),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Theme.of(context).primaryColor,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: FittedBox(child: Text('R\$${tr.value}')),
-                        ),
-                      ),
-                      title: Text(
-                        tr.title,
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      subtitle: Text(DateFormat('d MMM y').format(tr.date)),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        color: Theme.of(context).errorColor,
-                        onPressed: () => removeTransaction(tr.id),
-                      ),
-                    ),
-                  );
-                }));
+                  ),
+                  title: Text(
+                    tr.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(DateFormat('d MMM y').format(tr.date)),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => removeTransaction(tr.id),
+                  ),
+                ),
+              );
+            });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -71,27 +71,35 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      backgroundColor: Theme.of(context).primaryColor,
+      title: const Text('Despesas Pessoais'),
+      actions: [
+        IconButton(
+            onPressed: () => _openTransactionFormModal(context),
+            icon: Icon(Icons.add))
+      ],
+    );
+
+    final availableHeight = MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        title: const Text('Despesas Pessoais'),
-        actions: [
-          IconButton(
-              onPressed: () => _openTransactionFormModal(context),
-              icon: Icon(Icons.add))
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Chart(recentTrasaction: _recentTransactions),
-            Column(
-              children: [
-                TransactionList(
-                    transactions: _transactions,
-                    removeTransaction: _removeTransaction),
-              ],
+            Container(
+              height: availableHeight * 0.3,
+              child: Chart(recentTrasaction: _recentTransactions),
+            ),
+            Container(
+              height: availableHeight * 0.7,
+              child: TransactionList(
+                  transactions: _transactions,
+                  removeTransaction: _removeTransaction),
             )
           ],
         ),
